a2cdcdd8d99a3be7dc4cf72b703cbdae484ad1a2
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2021-03-04 14:00:27 +0100

    Little cleanup

--- TMF/internal/scripts/variables.ps1
+++ TMF/internal/scripts/variables.ps1
@@ -1,15 +1,15 @@
 # All required runtime variables.
-[regex] $guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
-[regex] $upnRegex = '^([A-Za-z\d\.]*)@([A-Za-z\d\.-]*)$'
-
 $graphVersionRequired = "beta"
 $script:graphBaseUrl = "https://graph.microsoft.com/{0}" -f $graphVersionRequired
+
+[regex] $script:guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
+[regex] $script:upnRegex = '^([A-Za-z\d\.]*)@([A-Za-z\d\.-]*)$'
 
 $script:supportedComponents = @{
     "groups" = (Get-Command Register-TmfGroup)
     "namedLocations" = (Get-Command Register-TmfNamedLocation)
-}
+} # All currently supported components.
 Set-Variable -Name supportedComponents -Option ReadOnly
 
-$script:activatedConfigurations = @() # Overview of all activated configurations
-$script:desiredConfiguration = @{} # The desired configuration. Contains the definitions for each object type.
+$script:activatedConfigurations = @() # Overview of all activated configurations.
+$script:desiredConfiguration = @{} # The desired configuration.
