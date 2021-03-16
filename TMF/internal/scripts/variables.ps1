bf2937d613f914e6595fcc57c093baf4df32a01d
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2021-03-16 16:12:57 +0100

    Renamed script variable supportedComponents to supportedResources. Also added nested hashtable with all required functions for a resource to supportedResources.

--- TMF/internal/scripts/variables.ps1
+++ TMF/internal/scripts/variables.ps1
@@ -5,13 +5,31 @@ $script:graphBaseUrl = "https://graph.microsoft.com/{0}" -f $graphVersionRequire
 [regex] $script:guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
 [regex] $script:upnRegex = '^([A-Za-z\d\.]*)@([A-Za-z\d\.-]*)$'
 
-$script:supportedComponents = @{
-    "stringMappings" = (Get-Command Register-TmfStringMapping)
-    "groups" = (Get-Command Register-TmfGroup)
-    "namedLocations" = (Get-Command Register-TmfNamedLocation)
-    "agreements" = (Get-Command Register-TmfAgreement)
+$script:supportedResources = @{
+    "stringMappings" = @{
+        "registerFunction" = (Get-Command Register-TmfStringMapping)
+        "weight" = 0
+    }    
+    "groups" = @{
+        "registerFunction" = (Get-Command Register-TmfGroup)
+        "testFunction" = (Get-Command Test-TmfGroup)
+        "invokeFunction" = (Get-Command Invoke-TmfGroup)
+        "weight" = 10
+    }
+    "namedLocations" = @{
+        "registerFunction" = (Get-Command Register-TmfNamedLocation)
+        "testFunction" = (Get-Command Test-TmfNamedLocation)
+        "invokeFunction" = (Get-Command Invoke-TmfNamedLocation)
+        "weight" = 10
+    }
+    "agreements" = @{
+        "registerFunction" = (Get-Command Register-TmfAgreement)
+        "testFunction" = (Get-Command Test-TmfAgreement)
+        "invokeFunction" = (Get-Command Invoke-TmfAgreement)
+        "weight" = 10
+    }
 } # All currently supported components.
-Set-Variable -Name supportedComponents -Option ReadOnly
+Set-Variable -Name supportedResources -Option ReadOnly
 
 $script:activatedConfigurations = @() # Overview of all activated configurations.
 
