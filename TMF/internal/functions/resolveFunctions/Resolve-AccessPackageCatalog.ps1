275f886bd4b510bcb6bee3002a2bc5bd136dabf5
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2021-04-20 10:27:14 +0200

    Replaced variable $group with $catalog

--- TMF/internal/functions/resolveFunctions/Resolve-AccessPackageCatalog.ps1
+++ TMF/internal/functions/resolveFunctions/Resolve-AccessPackageCatalog.ps1
@@ -15,15 +15,15 @@ function Resolve-AccessPackageCatalog
 	{			
 		try {
 			if ($InputReference -match $script:guidRegex) {
-				$group = (Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackageCatalogs/{0}" -f $InputReference)).Value
+				$catalog = (Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackageCatalogs/{0}" -f $InputReference)).Value
 			}
 			else {
-				$group = (Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackageCatalogs/?`$filter=displayName eq '{0}'" -f $InputReference)).Value
+				$catalog = (Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackageCatalogs/?`$filter=displayName eq '{0}'" -f $InputReference)).Value
 			}
 
-			if (!$group) { throw "Cannot find accessPackageCatalog $InputReference" }
-			if ($group.count -gt 1) { throw "Got multiple accessPackageCatalogs for $InputReference" }
-			return $group.Id
+			if (!$catalog){ throw "Cannot find accessPackageCatalog $InputReference" }
+			if ($catalog.count -gt 1) { throw "Got multiple accessPackageCatalogs for $InputReference" }
+			return $catalog.Id
 		}
 		catch {
 			Write-PSFMessage -Level Warning -String 'TMF.CannotResolveResource' -StringValues "AccessPackageCatalog" -Tag 'failed' -ErrorRecord $_
