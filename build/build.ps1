ef1574b143d5856cfb799512252250b7cdc2a207
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2021-05-05 09:18:20 +0200

    Added simple build script for TMF

---
+++ build/build.ps1
@@ -0,0 +1,26 @@
+<#
+    .SYNOPSIS
+        A simple build script for the Tenant Management Framework
+    .PARAMETER buildVersion
+        Provide a version in the format "{MAJOR}.{MINOR}.{PATCH}"
+        Example: 1.0.1
+    .PARAMETER moduleName
+        Provide the name of the Powershell Module.
+        Example: TMF
+    .PARAMETER modulePath
+        Path to the module directory.
+#>
+
+Param(
+    [string] $buildVersion = $env:BUILDVER,
+    [string] $moduleName = "TMF",
+    [string] $modulePath = "$PSScriptRoot\..\TMF"
+)
+
+$manifestPath = Join-Path -Path $modulePath -ChildPath "$moduleName.psd1"
+
+## Update build version in manifest
+$manifestContent = Get-Content -Path $manifestPath -Raw
+$manifestContent = $manifestContent -replace "ModuleVersion = '[\d]+.[\d]+.[\d]+'", ("ModuleVersion = '{0}'" -f $buildVersion)
+
+$manifestContent | Set-Content -Path $manifestPath
