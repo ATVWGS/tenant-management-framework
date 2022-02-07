f4095c4d11f4c5f04c0a36fe902b036cb279b9e1
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2022-02-07 12:56:29 +0100

    Fixed additional merge conflicts

--- TMF/internal/functions/resolve/Resolve-AccessPackage.ps1
+++ TMF/internal/functions/resolve/Resolve-AccessPackage.ps1
@@ -17,12 +17,7 @@ function Resolve-AccessPackage
 	{			
 		try {
 			if ($InputReference -match $script:guidRegex) {
-<<<<<<< HEAD
 				$package = (Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackages/{0}" -f $InputReference)).Value.Id
-=======
-				$providedId = $true
-				$package = Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackages/{0}" -f $InputReference)
->>>>>>> prepare-release/v1.3
 			}
 			else {
 				$package = (Invoke-MgGraphRequest -Method GET -Uri ("$script:graphBaseUrl/identityGovernance/entitlementManagement/accessPackages/?`$filter=displayName eq '{0}'" -f $InputReference)).Value.Id
@@ -37,17 +32,7 @@ function Resolve-AccessPackage
 			if (-Not $package -and -Not $DontFailIfNotExisting){ throw "Cannot find accessPackage $InputReference" }
 			elseif (-Not $package -and $DontFailIfNotExisting) { return }
 
-<<<<<<< HEAD
 			if ($package.count -gt 1) { throw "Got multiple accessPackages for $InputReference" }
 			return $package
-=======
-			if ($package.count -gt 1 -and -not $providedId) { throw "Got multiple accessPackages for $InputReference" }
-			return $package.Id
->>>>>>> prepare-release/v1.3
-		}
-		catch {
-			Write-PSFMessage -Level Warning -String 'TMF.CannotResolveResource' -StringValues "AccessPackage" -Tag 'failed' -ErrorRecord $_
-			$Cmdlet.ThrowTerminatingError($_)				
-		}			
 	}
 }
