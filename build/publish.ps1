0b89f5d6e18091e8affa0dd13f0751853501fb94
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2022-02-08 09:11:52 +0100

    Updated pipeline and publish.ps1

--- build/publish.ps1
+++ build/publish.ps1
@@ -1,18 +1,14 @@
 Param (
-    [string] $ArtifactPath = "$PWD\_tenant-management-framework",
+    [string] $ArtifactPath,
     [string] $ModuleName = $env:moduleName,
-    [string] $ApiKey = $env:apiKey,
-    [string] $LicenseUri = $env:licenseUri,
-    [string] $ProjectUri = $env:projectUri,
-    [string[]] $Tags = $env:tags
+    [string] $ApiKey = $env:apiKey
 )
 
-#region Install dependencies
-Install-Module -Name PSFramework -Scope CurrentUser -Force -Repository PSGallery
-Install-Module -Name 'Microsoft.Graph.Authentication' -Scope CurrentUser -Force -Repository PSGallery
-#endregion
-
-$package = Get-ChildItem -Filter "*.nupkg" -Path $ArtifactPath -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-Copy-Item -Path $package.FullName -Destination "$PSScriptRoot\$ModuleName.zip" -Force
-Expand-Archive -Path "$PSScriptRoot\$ModuleName.zip" -DestinationPath "$PSScriptRoot\$ModuleName" -Force
-Publish-Module -Path "$PSScriptRoot\$ModuleName" -LicenseUri $LicenseUri -ProjectUri $ProjectUri -Tags $Tags -NuGetApiKey $ApiKey
+begin {
+    $package = Get-ChildItem -Filter "*.nupkg" -Path $ArtifactPath -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+    Copy-Item -Path $package.FullName -Destination "$PSScriptRoot\$ModuleName.zip" -Force
+    Expand-Archive -Path "$PSScriptRoot\$ModuleName.zip" -DestinationPath "$PSScriptRoot\$ModuleName" -Force
+}
+process {
+    Publish-Module -Path "$PSScriptRoot\$ModuleName" -NuGetApiKey $ApiKey
+}
