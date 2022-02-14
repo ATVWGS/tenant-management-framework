4602ad3a5387d2cea34f1c6b4f9ebe1d9540c4c1
Roland Wagner <roland.wagner@volkswagen-groupservices.com>
2022-02-14 15:39:37 +0100

    Changed variable componentName to resourceName in Test-TmfGroup

--- TMF/functions/groups/Test-TmfGroup.ps1
+++ TMF/functions/groups/Test-TmfGroup.ps1
@@ -162,7 +162,7 @@
 					}
 				}
 				default {
-					Write-PSFMessage -Level Warning -String 'TMF.Test.MultipleResourcesError' -StringValues $componentName, $definition.displayName -Tag 'failed'
+					Write-PSFMessage -Level Warning -String 'TMF.Test.MultipleResourcesError' -StringValues $resourceName, $definition.displayName -Tag 'failed'
 					$exception = New-Object System.Data.DataException("Query returned multiple results. Cannot decide which resource to test.")
 					$errorID = 'MultipleResourcesError'
 					$category = [System.Management.Automation.ErrorCategory]::NotSpecified
