ba6c3dc090953c1ef8e222e8951b28062155a783
Seitle, Johannes (VWGS) <johannes.seitle@volkswagen-groupservices.com>
2022-03-29 11:07:43 +0200

    Changed build scripts

--- build/publish.ps1
+++ build/publish.ps1
@@ -7,16 +7,6 @@ Param (
 begin {
     $package = Get-ChildItem -Filter "*.zip" -Path $ArtifactPath -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1    
     Expand-Archive -Path $package.FullName -DestinationPath "$PSScriptRoot\$ModuleName" -Force
-    $manifest = Import-LocalizedData -BaseDirectory "$PSScriptRoot\$ModuleName" -FileName "$ModuleName.psd1"
-
-    #region Install dependencies
-    foreach ($module in $manifest.RequiredModules) {
-        switch ($module.GetType().Name) {
-            "String" { Install-Module -Name $module -Scope CurrentUser -Force -Repository PSGallery }
-            "Hashtable" { Install-Module -Name $module["ModuleName"] -RequiredVersion $module["RequiredVersion"] -Scope CurrentUser -Force -Repository PSGallery }
-        }
-    }
-    #endregion
 }
 process {
     Publish-Module -Path "$PSScriptRoot\$ModuleName" -NuGetApiKey $ApiKey
