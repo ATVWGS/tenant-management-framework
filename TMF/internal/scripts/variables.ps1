fe0d8b32accb68bf1d1a45d95e638e7be71a2d59
Roland Wagner <roland.wagner@volkswagen-groupservices.com>
2022-10-28 09:07:35 +0200

    Added variable apiVersion

--- TMF/internal/scripts/variables.ps1
+++ TMF/internal/scripts/variables.ps1
@@ -2,6 +2,7 @@
 $graphVersionRequired = "beta"
 $script:graphBaseUrl = "https://graph.microsoft.com/{0}" -f $graphVersionRequired
 $script:apiBaseUrl = "https://management.azure.com/"
+$script:apiVersion = "api-version=2022-01-01"
 
 [regex] $script:guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
 [regex] $script:upnRegex = '^([A-Za-z\d\.\-]*)@([A-Za-z\d\.-]*)$'
