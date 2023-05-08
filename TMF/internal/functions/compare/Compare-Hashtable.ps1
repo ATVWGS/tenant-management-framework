7862c5fc888381a643c610fb652467b32b5a6368
Roland Wagner <roland.wagner@volkswagen-groupservices.com>
2023-05-08 16:03:06 +0200

    Undo changes to Compare-Hashtable.ps1

--- TMF/internal/functions/compare/Compare-Hashtable.ps1
+++ TMF/internal/functions/compare/Compare-Hashtable.ps1
@@ -16,38 +16,32 @@ function Compare-Hashtable {
             if ($DifferenceObject.ContainsKey($reference.Key)) {     
                 if ($null -eq $reference.Value) {
                     if (-not ($null -eq $DifferenceObject[$reference.Key])) {
-                        Write-Host ("$($reference.Key) Line 19")
                         $same = $false
                     }
                 }
                 elseif ($reference.Value.GetType().Name -eq "Hashtable") {
                     if ($DifferenceObject[$reference.Key]) {
                         if (-Not (Compare-Hashtable -ReferenceObject $reference.Value -DifferenceObject $DifferenceObject[$reference.Key])) {
-                            Write-Host ("$($reference.Key) Line 26")
                             $same = $false
                         }
                     }
                     else {
-                        Write-Host ("$($reference.Key) Line 31")
                         $same = $false
                     }
                 }
-                elseif ($reference.Value.GetType().Name -eq "Object[]") {
+                elseif ($reference.Value.GetType() -in @("System.Object[]", "string[]")) {
                     if (Compare-Object -ReferenceObject $reference.Value -DifferenceObject $DifferenceObject[$reference.Key]) {
-                        Write-Host ("$($reference.Key) Line 37")
                         $same = $false
                     }
                 }
                 else {
                     if ($reference.Value -ne $DifferenceObject[$reference.Key]) {
-                        Write-Host ("$($reference.Key) Line 43")
                         $same = $false
                     }
                 }
             }
             else {
                 if (-not ($null -eq $reference.Value)) {
-                    Write-Host ("$($reference.Key) Line 50")
                     $same = $false
                 }                
             }            
