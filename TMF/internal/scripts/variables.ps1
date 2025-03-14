9ee4d48660e7823f0a7e098351beae132df1094b
PA-ADM Wagner <pa_av01f6f@volkswagengroup.onmicrosoft.com>
2025-03-14 07:15:44 +0000

    Correct upnRegex to fit all mail addresses

--- TMF/internal/scripts/variables.ps1
+++ TMF/internal/scripts/variables.ps1
@@ -6,7 +6,7 @@ $script:apiBaseUrl = "https://management.azure.com/"
 $script:apiVersion = "api-version=2022-01-01"
 
 [regex] $script:guidRegex = '(?im)^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$'
-[regex] $script:upnRegex = '^([A-Za-z\d\.\-]*)@([A-Za-z\d\.-]*)$'
+[regex] $script:upnRegex = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
 [regex] $script:mailNicknameRegex = '^([A-Za-z\d\.]*)@([A-Za-z\d\.-]*)$'
 [regex] $script:asterisk = '^\*?\w+\*?$'
 
