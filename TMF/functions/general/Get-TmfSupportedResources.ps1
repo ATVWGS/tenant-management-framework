08ac99ecdb72402414d624fba5c2599cb63924e9
PA-ADM Wagner <pa_av01f6f@volkswagengroup.onmicrosoft.com>
2026-02-04 13:59:37 +0100

    Added export of parentType for supported resources

--- TMF/functions/general/Get-TmfSupportedResources.ps1
+++ TMF/functions/general/Get-TmfSupportedResources.ps1
@@ -11,16 +11,29 @@ function Get-TmfSupportedResources
 	{
 
         $supportedResourcesExport = @()
-        $resourceNames = ($script:supportedResources).GetEnumerator().Name
+        $resourceNames = ($script:supportedResources).GetEnumerator().Name | Where-Object {$_ -notin @("stringMappings","roleManagementPolicyRuleTemplates")}
 
         foreach ($resourceName in $resourceNames) {
-            $supportedResourcesExport += [PSCustomObject]@{
-                Name = $resourceName
-                weight = ($script:supportedResources)[$resourceName].weight
-                ExportFunction = ($script:supportedResources)[$resourceName].ExportFunction.Name
-                InvokeFunction = ($script:supportedResources)[$resourceName].InvokeFunction.Name
-                TestFunction = ($script:supportedResources)[$resourceName].TestFunction.Name
-            }
+            $supportedResourcesExport += 
+                if (($script:supportedResources)[$resourceName].parentType) {
+                    [PSCustomObject]@{
+                        Name = $resourceName
+                        weight = ($script:supportedResources)[$resourceName].weight
+                        ExportFunction = ($script:supportedResources)[$resourceName].ExportFunction.Name
+                        InvokeFunction = ($script:supportedResources)[$resourceName].InvokeFunction.Name
+                        TestFunction = ($script:supportedResources)[$resourceName].TestFunction.Name
+                        parentType = ($script:supportedResources)[$resourceName].parentType
+                    }
+                }
+                else {
+                    [PSCustomObject]@{
+                        Name = $resourceName
+                        weight = ($script:supportedResources)[$resourceName].weight
+                        ExportFunction = ($script:supportedResources)[$resourceName].ExportFunction.Name
+                        InvokeFunction = ($script:supportedResources)[$resourceName].InvokeFunction.Name
+                        TestFunction = ($script:supportedResources)[$resourceName].TestFunction.Name
+                    }
+                }
         }
 		return $supportedResourcesExport
 	}
